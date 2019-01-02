a9fa5c9b556f08d1e02fa04455a7da93a6e3a496
Matt Crowe <github.mpc@zym.me>
2019-01-02 20:06:15 +0000

    tests

--- tests/Services/AlertServiceTest.php
+++ tests/Services/AlertServiceTest.php
@@ -5,6 +5,7 @@ use Mockery as m;
 use Belt\Notify\Alert;
 use Belt\Notify\Services\AlertService;
 use Belt\Core\Testing;
+use Belt\Core\Facades\TranslateFacade as Translate;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
@@ -18,12 +19,22 @@ class AlertServiceTest extends Testing\BeltTestCase
 
     /**
      * @covers \Belt\Notify\Services\AlertService::init
+     * @covers \Belt\Notify\Services\AlertService::cacheKey
      * @covers \Belt\Notify\Services\AlertService::cache
      * @covers \Belt\Notify\Services\AlertService::query
+     * @covers \Belt\Notify\Services\AlertService::clearCache
      */
     public function test()
     {
         $this->enableI18n();
+
+        # cacheKey
+        $service = new AlertService();
+        $this->assertEquals('alerts-foo', $service->cacheKey('foo'));
+        Translate::setLocale('es_ES');
+        $this->assertEquals('alerts-es_ES', $service->cacheKey());
+        Translate::setLocale('en_US');
+        $this->assertEquals('alerts-en_US', $service->cacheKey());
 
         # init
         Cache::shouldReceive('has')->with('alerts-en_US')->andReturn(false);
@@ -40,19 +51,36 @@ class AlertServiceTest extends Testing\BeltTestCase
         $alerts = new Collection();
         $alerts->add(factory(Alert::class)->make(['id' => 3]));
         $alerts->add(factory(Alert::class)->make(['id' => 2]));
-
         $query = m::mock(Builder::class);
         $query->shouldReceive('active')->andReturnSelf();
         $query->shouldReceive('get')->andReturn($alerts);
-
         $service = m::mock(AlertService::class . '[query]');
         $service->shouldReceive('query')->andReturn($query);
-
-        //$alerts = $alerts->keyBy('id');
-
         Cache::shouldReceive('put');
-
         $service->cache();
+
+        # clearCache
+        Cache::shouldReceive('delete')->with('alerts-es_ES')->andReturnSelf();
+        Cache::shouldReceive('delete')->with('alerts-en_US')->andReturnSelf();
+        $service->clearCache();
+
+    }
+
+    /**
+     * @covers \Belt\Notify\Services\AlertService::init
+     * @covers \Belt\Notify\Services\AlertService::cacheKey
+     * @covers \Belt\Notify\Services\AlertService::cache
+     * @covers \Belt\Notify\Services\AlertService::query
+     * @covers \Belt\Notify\Services\AlertService::clearCache
+     */
+    public function ztest2()
+    {
+        $this->enableI18n();
+
+        # cacheKey
+        $service = new AlertService();
+        Translate::shouldReceive('getLocale')->once()->andReturn('es_ES');
+        $this->assertEquals('alerts-es_ES', $service->cacheKey());
 
     }
 
