9ce39e98cb132b23c7ea21241a67d237f6476812
Matt Crowe <github.mpc@zym.me>
2019-02-12 18:47:22 +0000

    continue unified documentation

--- resources/views/docs/20/admin/notify/alerts/intro.blade.php
+++ resources/views/docs/20/admin/notify/alerts/intro.blade.php
@@ -2,12 +2,16 @@
 
 To add an Alert to the site, go to Alerts → Add Alerts
 
-* Check "Is Active"
-* **Name** Name of Alert
-* **Starts at** indicate date and time you want Alert to start
-* **Ends at** indicate date and time you want Alert to end
-* **Intro** Leave blank
-* **Body** Body appears in alert on front end
+@include('belt-docs::partials.table', [
+    'rows' => [
+        ['Is Active', 'Check to make the Alert page publicly available.'],
+        ['Name', 'Name of Alert'],
+        ['Starts at', 'Indicate date and time you want Alert to start'],
+        ['Ends at', 'Indicate date and time you want Alert to end'],
+        ['Intro', 'Leave blank'],
+        ['Body', 'Body appears in alert on front end'],
+    ],
+])
 
 @include('belt-docs::partials.image', ['src' => '20/admin/notify/assets/alert-example.png'])
 
